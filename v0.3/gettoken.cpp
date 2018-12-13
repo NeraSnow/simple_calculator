c7e330af3fb47beeac80ab24f95af23b66677ccb
Benjamin Chen <35217918+NeraSnow@users.noreply.github.com>
2018-12-13 20:06:46 +0800

    Update gettoken

---
+++ v0.3/gettoken.cpp
@@ -0,0 +1,12 @@
+// gettoken.cpp
+
+#include "gettoken.h"
+#include <iostream>
+
+using namespace std;
+
+// Example (+ 2 3)
+string gettoken(string str)
+{
+    
+}
---
+++ v0.3/gettoken.h
@@ -0,0 +1,13 @@
+// gettoken.h
+
+#ifndef GETTOKEN_H
+#define GETTOKEN_H
+
+#include <string>
+
+using namespace std;
+
+class 
+
+string gettoken(string str);
+
