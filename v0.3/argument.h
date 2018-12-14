41f0a4f944f01ed8fd9c11f13a648eef5fce1820
Junze Zhang <zhanjunz@flip2.engr.oregonstate.edu>
2018-12-13 21:58:21 -0800

    add argument.h

---
+++ v0.3/argument.h
@@ -0,0 +1,15 @@
+#ifndef ARGUMENT_H
+#define ARGUMENT_H
+#include "node.h"
+
+class Argument{
+
+	public:
+		Argument();
+		Argument(const Argument&);
+		Argument& operator = (const Argument&);
+		~Argument();
+
+}
+
+#endif
